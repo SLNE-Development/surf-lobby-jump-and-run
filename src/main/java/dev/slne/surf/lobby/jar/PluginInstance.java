0ccdaa492132e565db2b2c0aa7681034bc3d54eb
Red <red.dev@hexle.at>
2024-11-03 00:24:33 +0100

    feat: plugin class structure

--- src/main/java/dev/slne/surf/lobby/jar/PluginInstance.java
+++ src/main/java/dev/slne/surf/lobby/jar/PluginInstance.java
@@ -1,7 +1,23 @@
 package dev.slne.surf.lobby.jar;
 
+import dev.slne.surf.lobby.jar.command.ParkourCommand;
+import lombok.Getter;
+import lombok.experimental.Accessors;
 import org.bukkit.plugin.java.JavaPlugin;
 
+@Getter
+@Accessors(fluent = true)
 public class PluginInstance extends JavaPlugin {
+  private JumpAndRunProvider jumpAndRunProvider;
 
+  @Override
+  public void onEnable() {
+    this.jumpAndRunProvider = new JumpAndRunProvider();
+
+    new ParkourCommand("parkour").register();
+  }
+
+  public static PluginInstance instance(){
+    return getPlugin(PluginInstance.class);
+  }
 }
