3ccd7fc7fed7e690b1f3b7f808891a60af5e4dc1
Red <red.dev@hexle.at>
2024-11-06 18:18:16 +0100

    feat: remove debug commands

--- src/main/java/dev/slne/surf/lobby/jar/command/ParkourCommand.java
+++ src/main/java/dev/slne/surf/lobby/jar/command/ParkourCommand.java
@@ -3,7 +3,6 @@ package dev.slne.surf.lobby.jar.command;
 import dev.jorel.commandapi.CommandAPICommand;
 import dev.slne.surf.lobby.jar.command.subcommand.ParkourListCommand;
 import dev.slne.surf.lobby.jar.command.subcommand.ParkourStartCommand;
-import dev.slne.surf.lobby.jar.command.subcommand.debug.ParkourDebugCommand;
 import dev.slne.surf.lobby.jar.command.subcommand.setting.ParkourSettingCommand;
 
 public class ParkourCommand extends CommandAPICommand {
@@ -13,7 +12,6 @@ public class ParkourCommand extends CommandAPICommand {
 
     withPermission("surf.lobby.jar.jumpandrun.command");
 
-    withSubcommand(new ParkourDebugCommand("debug"));
     withSubcommand(new ParkourSettingCommand("setting"));
     withSubcommand(new ParkourStartCommand("start"));
     withSubcommand(new ParkourListCommand("list"));
