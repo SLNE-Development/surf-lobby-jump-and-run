4289c8dd247ab70b32721e34b2a8fa3e2fac5d65
Red <red.dev@hexle.at>
2024-11-07 14:31:26 +0100

    feat: parkour start cmd

---
+++ src/main/java/dev/slne/surf/lobby/jar/command/subcommand/setting/ParkourSettingStartCommand.java
@@ -0,0 +1,24 @@
+package dev.slne.surf.lobby.jar.command.subcommand.setting;
+
+import dev.jorel.commandapi.CommandAPICommand;
+import dev.jorel.commandapi.arguments.LocationArgument;
+import dev.slne.surf.lobby.jar.PluginInstance;
+import net.kyori.adventure.text.Component;
+import org.bukkit.Location;
+
+public class ParkourSettingStartCommand extends CommandAPICommand {
+
+  public ParkourSettingStartCommand(String commandName) {
+    super(commandName);
+
+    withArguments(new LocationArgument("pos"));
+
+    executesPlayer((player, args) -> {
+      Location pos = args.getUnchecked("pos");
+
+      PluginInstance.instance().jumpAndRunProvider().jumpAndRun().setStart(pos);
+
+      player.sendMessage(PluginInstance.prefix().append(Component.text("Du hast den Start erfolgreich neu definiert.")));
+    });
+  }
+}
