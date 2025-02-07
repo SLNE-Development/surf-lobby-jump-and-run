13e32e67707e6bfcb4f9b169d35ca51e58b30cde
TheBjoRedCraft <red.dev@hexle.at>
2025-02-07 21:58:43 +0100

    feat: use hikaricp + activejdbc in db and cleanup

--- src/main/java/dev/slne/surf/lobby/jar/mysql/Database.kt
+++ src/main/java/dev/slne/surf/lobby/jar/mysql/Database.kt
@@ -1,16 +1,28 @@
 package dev.slne.surf.lobby.jar.mysql
 
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
 import dev.slne.surf.lobby.jar.plugin
-
 import it.unimi.dsi.fastutil.objects.Object2ObjectMap
 import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap
-
 import org.bukkit.Bukkit
 import org.javalite.activejdbc.Base
 import org.javalite.activejdbc.Model
 import java.util.*
+import javax.sql.DataSource
 
 object Database {
+    private const val DRIVER_CLASS_NAME_KEY = "mysql.driver"
+    private const val POOL_NAME_KEY = "mysql.poolName"
+    private const val URL_KEY = "mysql.url"
+    private const val DB_TYPE_KEY = "mysql.type"
+    private const val HOSTNAME_KEY = "mysql.hostname"
+    private const val PORT_KEY = "mysql.port"
+    private const val DATABASE_NAME_KEY = "mysql.database"
+    private const val USERNAME_KEY = "mysql.username"
+    private const val PASSWORD_KEY = "mysql.password"
+    private const val TABLE_KEY = "mysql.table"
+
     private var driverClassName = "org.mariadb.jdbc.Driver"
     private var poolName = "surf-lobby-jnr"
     private var jdbcUrl = "url"
@@ -19,33 +31,48 @@ object Database {
     private var table = "table"
     private var url = "url"
 
+    private lateinit var dataSource: DataSource
+
+    val highScores: Object2ObjectMap<UUID, Int> get() = getValues("high_score")
+    val points: Object2ObjectMap<UUID, Int> get() = getValues("points")
+
     init {
         val config = plugin.config
-        val dbType = config.getString("mysql.type", "mariadb")
-        val hostname = config.getString("mysql.hostname")
-        val port = config.getInt("mysql.port")
-        val databaseName = config.getString("mysql.database")
+        val dbType = config.getString(DB_TYPE_KEY, "mariadb")
+        val hostname = config.getString(HOSTNAME_KEY)
+        val port = config.getInt(PORT_KEY)
+        val databaseName = config.getString(DATABASE_NAME_KEY)
 
-        driverClassName = config.getString("mysql.driver", "org.mariadb.jdbc.Driver") ?: "org.mariadb.jdbc.Driver"
-        poolName = config.getString("mysql.poolName", "surf-lobby-jnr") ?: "something"
-        url = config.getString("mysql.url") ?: "url"
+        driverClassName = config.getString(DRIVER_CLASS_NAME_KEY, driverClassName) ?: driverClassName
+        poolName = config.getString(POOL_NAME_KEY, poolName) ?: poolName
+        url = config.getString(URL_KEY) ?: url
 
         jdbcUrl = "jdbc:$dbType://$hostname:$port/$databaseName"
-        username = config.getString("mysql.username") ?: "user"
-        password = config.getString("mysql.password") ?: "password"
-        table = config.getString("mysql.table") ?: "table"
+        username = config.getString(USERNAME_KEY) ?: username
+        password = config.getString(PASSWORD_KEY) ?: password
+        table = config.getString(TABLE_KEY) ?: table
+
+        setupDataSource()
+    }
+
+    private fun setupDataSource() {
+        val hikariConfig = HikariConfig().apply {
+            jdbcUrl = this@Database.jdbcUrl
+            driverClassName = this@Database.driverClassName
+            username = this@Database.username
+            password = this@Database.password
+            poolName = this@Database.poolName
+            maximumPoolSize = 10
+            minimumIdle = 2
+            idleTimeout = 30000
+            connectionTimeout = 30000
+        }
+        dataSource = HikariDataSource(hikariConfig)
     }
 
     fun createConnection() {
-        val finalUrl: String = if(url == "url") {
-            jdbcUrl
-        } else {
-            url
-        }
-
         try {
-            Base.open(driverClassName, finalUrl, username, password)
-
+            Base.open(dataSource)
             createTable()
         } catch (e: Exception) {
             Bukkit.getConsoleSender().sendMessage(e.message ?: this.javaClass.toString())
@@ -61,7 +88,7 @@ object Database {
                 sound TINYINT(1) DEFAULT TRUE,
                 high_score INT DEFAULT 0
             )
-            """.trimIndent()
+        """.trimIndent()
 
         try {
             Base.exec(query)
@@ -76,152 +103,47 @@ object Database {
         }
     }
 
-    suspend fun getTrys(uuid: UUID): Int {
-        var trys: Int? = null
+    fun getTrys(uuid: UUID): Int = getValue(uuid, "trys")
+    fun getPoints(uuid: UUID): Int = getValue(uuid, "points")
+    fun getHighScore(uuid: UUID): Int = getValue(uuid, "high_score")
+    fun getSound(uuid: UUID): Boolean = getValue(uuid, "sound") == 1
+    fun saveSound(uuid: UUID, value: Boolean?) = saveValue(uuid, "sound", value)
+    fun savePoints(uuid: UUID, points: Int?) = saveValue(uuid, "points", points)
+    fun saveTrys(uuid: UUID, trys: Int?) = saveValue(uuid, "trys", trys)
+    fun saveHighScore(uuid: UUID, highScore: Int?) = saveValue(uuid, "high_score", highScore)
 
-        try {
-            val result = Model.findFirst<JumpAndRunModel>("uuid = ?", uuid.toString())
-            if (result != null) {
-                trys = result.getInteger("trys")
-            }
+    private fun getValue(uuid: UUID, column: String): Int {
+        return try {
+            Model.findFirst<JumpAndRunModel>("uuid = ?", uuid.toString())?.getInteger(column) ?: 0
         } catch (e: Exception) {
             Bukkit.getConsoleSender().sendMessage(e.message ?: this.javaClass.toString())
+            0
         }
-
-        return trys ?: 0
     }
 
-    fun getPoints(uuid: UUID): Int {
-        var points: Int? = null
-
+    private fun saveValue(uuid: UUID, column: String, value: Any?) {
         try {
-            val result = Model.findFirst<JumpAndRunModel>("uuid = ?", uuid.toString())
-            if (result != null) {
-                points = result.getInteger("points")
+            val model = Model.findFirst("uuid = ?", uuid.toString()) ?: JumpAndRunModel().apply {
+                set<Model>("uuid", uuid.toString())
             }
-        } catch (e: Exception) {
-            Bukkit.getConsoleSender().sendMessage(e.message ?: this.javaClass.toString())
-        }
-
-        return points ?: 0
-    }
-
-    fun getHighScore(uuid: UUID): Int {
-        var highScore: Int? = null
-
-        try {
-            val result = Model.findFirst<JumpAndRunModel>("uuid = ?", uuid.toString())
-            if (result != null) {
-                highScore = result.getInteger("high_score")
-            }
-        } catch (e: Exception) {
-            Bukkit.getConsoleSender().sendMessage(e.message ?: this.javaClass.toString())
-        }
-
-        return highScore ?: 0
-    }
-
-    fun getSound(uuid: UUID): Boolean {
-        var sound = true
-
-        try {
-            val result = Model.findFirst<JumpAndRunModel>("uuid = ?", uuid.toString())
-            if (result != null) {
-                sound = result.getBoolean("sound")
-            }
-        } catch (e: Exception) {
-            Bukkit.getConsoleSender().sendMessage(e.message ?: this.javaClass.toString())
-        }
-
-        return sound
-    }
-
-    fun saveSound(uuid: UUID, value: Boolean?) {
-        try {
-            var model = Model.findFirst<JumpAndRunModel>("uuid = ?", uuid.toString())
-            if (model == null) {
-                model = JumpAndRunModel()
-                model.set<Model>("uuid", uuid.toString())
-            }
-
-            model.set<Model>("sound", value)
+            model.set<Model>(column, value)
             model.saveIt()
         } catch (e: Exception) {
             Bukkit.getConsoleSender().sendMessage(e.message ?: this.javaClass.toString())
         }
     }
 
-    fun savePoints(uuid: UUID, points: Int?) {
+    private fun getValues(column: String): Object2ObjectMap<UUID, Int> {
+        val values: Object2ObjectMap<UUID, Int> = Object2ObjectOpenHashMap()
         try {
-            var model = Model.findFirst<JumpAndRunModel>("uuid = ?", uuid.toString())
-            if (model == null) {
-                model = JumpAndRunModel()
-                model.set<Model>("uuid", uuid.toString())
+            for (result in Model.findAll<Model>()) {
+                values[UUID.fromString(result.getString("uuid"))] = result.getInteger(column)
             }
-
-            model.set<Model>("points", points)
-            model.saveIt()
         } catch (e: Exception) {
             Bukkit.getConsoleSender().sendMessage(e.message ?: this.javaClass.toString())
         }
+        return values
     }
-
-    fun saveTrys(uuid: UUID, trys: Int?) {
-        try {
-            var model = Model.findFirst<JumpAndRunModel>("uuid = ?", uuid.toString())
-            if (model == null) {
-                model = JumpAndRunModel()
-                model.set<Model>("uuid", uuid.toString())
-            }
-
-            model.set<Model>("trys", trys)
-            model.saveIt()
-        } catch (e: Exception) {
-            Bukkit.getConsoleSender().sendMessage(e.message ?: this.javaClass.toString())
-        }
-    }
-
-    fun saveHighScore(uuid: UUID, highScore: Int?) {
-        try {
-            var model = Model.findFirst<JumpAndRunModel>("uuid = ?", uuid.toString())
-            if (model == null) {
-                model = JumpAndRunModel()
-                model.set<Model>("uuid", uuid.toString())
-            }
-
-            model.set<Model>("high_score", highScore)
-            model.saveIt()
-        } catch (e: Exception) {
-            Bukkit.getConsoleSender().sendMessage(e.message ?: this.javaClass.toString())
-        }
-    }
-
-    val highScores: Object2ObjectMap<UUID, Int> get() {
-            val highScores: Object2ObjectMap<UUID, Int> = Object2ObjectOpenHashMap()
-
-            try {
-                for (result in Model.findAll<Model>()) {
-                    highScores[UUID.fromString(result.getString("uuid"))] =
-                        result.getInteger("high_score")
-                }
-            } catch (e: Exception) {
-                Bukkit.getConsoleSender().sendMessage(e.message ?: this.javaClass.toString())
-            }
-            return highScores
-        }
-
-    val points: Object2ObjectMap<UUID, Int> get() {
-            val points: Object2ObjectMap<UUID, Int> = Object2ObjectOpenHashMap()
-
-            try {
-                for (result in Model.findAll<Model>()) {
-                    points[UUID.fromString(result.getString("uuid"))] = result.getInteger("points")
-                }
-            } catch (e: Exception) {
-                Bukkit.getConsoleSender().sendMessage(e.message ?: this.javaClass.toString())
-            }
-            return points
-        }
 }
 
 internal class JumpAndRunModel : Model()
