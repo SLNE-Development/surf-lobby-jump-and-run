f0cf5cf36314f59d822380777233b932329b27b0
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-03-02 13:22:34 +0100

    feat: permissions

---
+++ src/main/java/dev/slne/surf/parkour/util/Permission.kt
@@ -0,0 +1,7 @@
+package dev.slne.surf.parkour.util
+
+object Permission {
+    const val COMMAND_PARKOUR_CREATE = "surf.parkour.command.create"
+    const val COMMAND_PARKOUR_DELETE = "surf.parkour.command.delete"
+    const val COMMAND_PARKOUR_START = "surf.parkour.command.start"
+}
