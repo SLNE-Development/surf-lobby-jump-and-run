2783de5f7f4a392b0ac7ac639988bd3f30878fc3
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-08-02 16:56:31 +0200

    feat: add import for ParkourPacketListener in BukkitListenerManager

--- surf-parkour-bukkit/src/main/kotlin/dev/slne/surf/parkour/BukkitListenerManager.kt
+++ surf-parkour-bukkit/src/main/kotlin/dev/slne/surf/parkour/BukkitListenerManager.kt
@@ -2,6 +2,7 @@ package dev.slne.surf.parkour
 
 import com.github.retrooper.packetevents.PacketEvents
 import com.github.retrooper.packetevents.event.PacketListenerPriority
+
 import dev.slne.surf.parkour.listener.ParkourPacketListener
 import dev.slne.surf.parkour.listener.PolarMitigationListener
 import dev.slne.surf.parkour.listener.VulcanEventListener
