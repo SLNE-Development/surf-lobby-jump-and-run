ad9939e984daf8e3fbf1945d28ad4098307f4659
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-08-02 16:42:29 +0200

    feat: remove unused DatabaseProvider import in FallbackDatabaseService

--- surf-parkour-fallback/src/main/kotlin/dev/slne/surf/parkour/fallback/service/FallbackDatabaseService.kt
+++ surf-parkour-fallback/src/main/kotlin/dev/slne/surf/parkour/fallback/service/FallbackDatabaseService.kt
@@ -1,11 +1,10 @@
 package dev.slne.surf.parkour.fallback.service
 
-import dev.slne.surf.database.database.DatabaseProvider
 import dev.slne.surf.parkour.core.service.DatabaseService
 
 class FallbackDatabaseService : DatabaseService {
     override fun connect() {
-        DatabaseProvider
+
     }
 
     override fun createTables() {
