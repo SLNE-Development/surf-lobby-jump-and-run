34e60822cc442f4836db307335c691bd3b3654c6
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-08-06 13:24:14 +0200

    feat: add FallbackParkourStatisticService implementation

---
+++ surf-parkour-fallback/src/main/kotlin/dev/slne/surf/parkour/fallback/service/FallbackParkourStatisticService.kt
@@ -0,0 +1,34 @@
+package dev.slne.surf.parkour.fallback.service
+
+import com.google.auto.service.AutoService
+import dev.slne.surf.parkour.api.model.parkour.Parkour
+import dev.slne.surf.parkour.api.model.parkour.statistic.ParkourStatistic
+import dev.slne.surf.parkour.core.service.ParkourStatisticsService
+import net.kyori.adventure.util.Services
+import java.util.*
+
+@AutoService(ParkourStatisticsService::class)
+class FallbackParkourStatisticService : ParkourStatisticsService, Services.Fallback {
+    override fun createTable() {
+
+    }
+
+    override suspend fun getStatistic(uuid: UUID): ParkourStatistic {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getParkourStatistic(
+        uuid: UUID,
+        parkour: Parkour
+    ): ParkourStatistic {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun addStatistic(
+        uuid: UUID,
+        parkour: Parkour,
+        statistic: ParkourStatistic
+    ) {
+        TODO("Not yet implemented")
+    }
+}
