a1e41459dfb5f338926bad78b53094f6217b87fb
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-09-04 22:10:18 +0200

    feat: remove UUID property from ParkourCreationData interface

--- surf-parkour-api/src/main/kotlin/dev/slne/surf/parkour/api/model/parkour/ParkourCreationData.kt
+++ surf-parkour-api/src/main/kotlin/dev/slne/surf/parkour/api/model/parkour/ParkourCreationData.kt
@@ -1,10 +1,8 @@
 package dev.slne.surf.parkour.api.model.parkour
 
 import org.bukkit.Location
-import java.util.*
 
 interface ParkourCreationData {
-    val uuid: UUID
     val name: String
     val corner1: Location
     val corner2: Location
