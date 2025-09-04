9ad49d5dc24d6dab83f61cb00aa79ce23e4a87c9
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-09-04 22:32:13 +0200

    feat: remove vertical jump options from DirectionalJump

--- surf-parkour-core/src/main/kotlin/dev/slne/surf/parkour/core/model/jump/Jumps.kt
+++ surf-parkour-core/src/main/kotlin/dev/slne/surf/parkour/core/model/jump/Jumps.kt
@@ -30,13 +30,6 @@ enum class Jumps(val jump: Jump) {
 //    FOUR_FORWARD_TWO_RIGHT(object : DirectionalJump(4, -2, 0) {}),
 //    FOUR_FORWARD_THREE_LEFT(object : DirectionalJump(4, 3, 0) {}),
 //    FOUR_FORWARD_THREE_RIGHT(object : DirectionalJump(4, -3, 0) {}),
-
-    ONE_FORWARD_UP(object : DirectionalJump(1, 0, 1) {}),
-    ONE_FORWARD_DOWN(object : DirectionalJump(1, 0, -1) {}),
-
-    TWO_FORWARD_UP(object : DirectionalJump(2, 0, 1) {}),
-    TWO_FORWARD_DOWN(object : DirectionalJump(2, 0, -1) {}),
-
     ONE_FORWARD_ONE_LEFT_UP(object : DirectionalJump(1, 1, 1) {}),
     ONE_FORWARD_ONE_RIGHT_UP(object : DirectionalJump(1, -1, 1) {}),
     ONE_FORWARD_ONE_LEFT_DOWN(object : DirectionalJump(1, 1, -1) {}),
