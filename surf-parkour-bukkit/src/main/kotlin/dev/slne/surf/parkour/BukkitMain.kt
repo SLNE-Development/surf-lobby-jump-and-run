c7d49a9d49ca245ce73f3772c0b3703478905b65
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-09-04 12:18:01 +0200

    feat: integrate Metrics for performance tracking and manage shutdown process

--- surf-parkour-bukkit/src/main/kotlin/dev/slne/surf/parkour/BukkitMain.kt
+++ surf-parkour-bukkit/src/main/kotlin/dev/slne/surf/parkour/BukkitMain.kt
@@ -2,19 +2,27 @@ package dev.slne.surf.parkour
 
 import com.github.shynixn.mccoroutine.folia.SuspendingJavaPlugin
 import dev.slne.surf.parkour.config.ParkourConfiguration
+import dev.slne.surf.surfapi.bukkit.api.metrics.Metrics
 import org.bukkit.plugin.java.JavaPlugin
+
+val plugin get() = JavaPlugin.getPlugin(BukkitMain::class.java)
+
+lateinit var metrics: Metrics
 
 class BukkitMain : SuspendingJavaPlugin() {
     override fun onEnable() {
         BukkitCommandManager.registerCommands()
         BukkitListenerManager.registerBukkitListeners()
         BukkitListenerManager.registerExternalListeners()
+
+        metrics = Metrics(this, 27168)
     }
 
     override fun onDisable() {
-        super.onDisable()
+        if (::metrics.isInitialized) {
+            metrics.shutdown()
+        }
     }
-}
 
-val plugin get() = JavaPlugin.getPlugin(BukkitMain::class.java)
-val parkourConfig = ParkourConfiguration()
+    val parkourConfig = ParkourConfiguration()
+}
