ee53a2ef3dc4a4c8b2305fe7457db08e462a7d8f
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-09-05 19:31:20 +0200

    feat: integrate player data insertion into parkour player creation

--- surf-parkour-fallback/src/main/kotlin/dev/slne/surf/parkour/fallback/factory/FallbackParkourPlayerFactory.kt
+++ surf-parkour-fallback/src/main/kotlin/dev/slne/surf/parkour/fallback/factory/FallbackParkourPlayerFactory.kt
@@ -4,6 +4,7 @@ import com.google.auto.service.AutoService
 import dev.slne.surf.parkour.api.entity.ParkourPlayer
 import dev.slne.surf.parkour.core.entity.CoreParkourPlayer
 import dev.slne.surf.parkour.core.factory.ParkourPlayerFactory
+import dev.slne.surf.parkour.core.service.parkourPlayerService
 import dev.slne.surf.parkour.core.util.loadProfileTexture
 import dev.slne.surf.surfapi.core.api.service.PlayerLookupService
 import net.kyori.adventure.util.Services
@@ -15,14 +16,18 @@ class FallbackParkourPlayerFactory : ParkourPlayerFactory, Services.Fallback {
         val name =
             PlayerLookupService.getUsername(uuid) ?: error("Player with UUID $uuid not found")
         val texture = loadProfileTexture(uuid)
+        val player = CoreParkourPlayer(uuid, name, texture)
 
-        return CoreParkourPlayer(uuid, name, texture)
+        parkourPlayerService.insertPlayer(player)
+        return player
     }
 
     override suspend fun createPlayer(name: String): ParkourPlayer {
         val uuid = PlayerLookupService.getUuid(name) ?: error("Player with name $name not found")
         val texture = loadProfileTexture(uuid)
+        val player = CoreParkourPlayer(uuid, name, texture)
 
-        return CoreParkourPlayer(uuid, name, texture)
+        parkourPlayerService.insertPlayer(player)
+        return player
     }
 }
