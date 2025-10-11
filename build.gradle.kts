59fa27c5e35973109c1e7b2fc2466e5ca5da72a8
Jo_field <150937665+JoField08@users.noreply.github.com>
2025-10-11 17:20:45 +0200

    chore: update surf-api-gradle-plugin and project version to 1.21.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,11 +4,11 @@ buildscript {
         maven("https://repo.slne.dev/repository/maven-public/") { name = "maven-public" }
     }
     dependencies {
-        classpath("dev.slne.surf:surf-api-gradle-plugin:1.21.7+")
+        classpath("dev.slne.surf:surf-api-gradle-plugin:1.21.10+")
     }
 }
 
 allprojects {
     group = "dev.slne.surf.parkour"
-    version = "1.21.8-2.0.0-SNAPSHOT"
+    version = "1.21.10-2.0.0-SNAPSHOT"
 }
