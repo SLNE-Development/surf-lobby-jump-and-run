27c38a189bc431b54386773ab54f5ce9ab271edd
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-11-03 18:19:14 +0100

    chore: set project version and group in build configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,9 @@ plugins {
     id("dev.slne.surf.surfapi.gradle.paper-plugin")
 }
 
+version = findProperty("version") as String
+group = "dev.slne.surf.parkour"
+
 surfPaperPluginApi {
     mainClass("dev.slne.surf.parkour.BukkitMain")
     foliaSupported(true)
