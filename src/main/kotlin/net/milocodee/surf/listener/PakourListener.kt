3517f1865c94d9c8f80b85c259de430065ccc0ff
Milo <meistertoast.discord@gmail.com>
2025-11-15 12:57:56 +0100

    Update PakourListener.kt

--- src/main/kotlin/net/milocodee/surf/listener/PakourListener.kt
+++ src/main/kotlin/net/milocodee/surf/listener/PakourListener.kt
@@ -1,52 +1,54 @@
 package net.milocodee.surf.listener
 
-import net.milocodee.surf.ActionbarDisplay
+import net.milocodee.surf.service.ActionbarService
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerQuitEvent
+import java.util.UUID
 
 class ParkourListener(
-    private val actionbarDisplay: ActionbarDisplay
 ) : Listener {
 
-    private val activeParkours: MutableMap<Player, ParkourSession> = mutableMapOf()
+    private val activeParkours = mutableMapOf<UUID, ParkourSession>()
 
     fun onParkourStart(player: Player, getCurrentJumps: () -> Int, highscore: Int) {
+        val uuid = player.uniqueId
         val session = ParkourSession(getCurrentJumps, highscore)
-        activeParkours[player] = session
+        activeParkours[uuid] = session
 
-        actionbarDisplay.startDisplay(player, getCurrentJumps, highscore)
+        ActionbarService.start(player, getCurrentJumps, highscore)
     }
 
     fun onParkourEnd(player: Player, finalJumps: Int): Boolean {
-        val session = activeParkours.remove(player) ?: return false
+        val uuid = player.uniqueId
+        val session = activeParkours.remove(uuid) ?: return false
 
         val isNewHighscore = finalJumps < session.highscore || session.highscore == 0
-
         if (isNewHighscore) {
-            actionbarDisplay.updateHighscore(player, finalJumps)
+            ActionbarService.updateHighscore(player, finalJumps)
         }
 
-        actionbarDisplay.stopDisplay(player)
-
+        ActionbarService.stop(player)
         return isNewHighscore
     }
 
     fun onParkourLeave(player: Player) {
-        activeParkours.remove(player)
-        actionbarDisplay.stopDisplay(player)
+        activeParkours.remove(player.uniqueId)
+        ActionbarService.stop(player)
     }
+
+    fun isPlayerActive(player: Player): Boolean {
+        return activeParkours.containsKey(player.uniqueId)
+    }
+
 
     @EventHandler
     fun onPlayerQuit(event: PlayerQuitEvent) {
-        val player = event.player
-        if (activeParkours.containsKey(player)) {
-            onParkourLeave(player)
-        }
+        onParkourLeave(event.player)
     }
 
-    data class ParkourSession(
+    private data class ParkourSession(
         val getCurrentJumps: () -> Int,
         val highscore: Int
     )
