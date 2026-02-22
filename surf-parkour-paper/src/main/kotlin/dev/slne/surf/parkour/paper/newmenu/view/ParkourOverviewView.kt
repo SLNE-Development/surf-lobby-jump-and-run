0b2a4701411f2c5e0fa8fb0c301c8f37457c3e6d
TheBjoRedCraft <thebjoredcraft@gmail.com>
2026-02-22 21:19:45 +0100

    feat: enhance ParkourOverviewView to start parkour and handle no available parkours

--- surf-parkour-paper/src/main/kotlin/dev/slne/surf/parkour/paper/newmenu/view/ParkourOverviewView.kt
+++ surf-parkour-paper/src/main/kotlin/dev/slne/surf/parkour/paper/newmenu/view/ParkourOverviewView.kt
@@ -1,11 +1,15 @@
 package dev.slne.surf.parkour.paper.newmenu.view
 
+import com.github.shynixn.mccoroutine.folia.launch
 import dev.slne.surf.parkour.paper.newmenu.util.MenuHeads
 import dev.slne.surf.parkour.paper.newmenu.util.outlineItem
+import dev.slne.surf.parkour.paper.plugin
+import dev.slne.surf.parkour.paper.service.parkourService
 import dev.slne.surf.surfapi.bukkit.api.builder.buildItem
 import dev.slne.surf.surfapi.bukkit.api.builder.displayName
 import dev.slne.surf.surfapi.bukkit.api.inventory.framework.titleBuilder
 import dev.slne.surf.surfapi.core.api.font.toSmallCaps
+import dev.slne.surf.surfapi.core.api.messages.adventure.sendText
 import me.devnatan.inventoryframework.View
 import me.devnatan.inventoryframework.ViewConfigBuilder
 import me.devnatan.inventoryframework.context.RenderContext
@@ -37,7 +41,19 @@ object ParkourOverviewView : View() {
         render.layoutSlot('L', leaderBoardItem).onClick { context ->
             context.openForPlayer(ParkourLeaderboardView::class.java)
         }
-        render.layoutSlot('S', startItem)
+        render.layoutSlot('S', startItem).onClick { context ->
+            val parkour = parkourService.getParkours().firstOrNull() ?: run {
+                context.player.sendText {
+                    appendErrorPrefix()
+                    error("Es sind derzeit keine Parkours verfügbar.")
+                }
+                return@onClick
+            }
+
+            plugin.launch {
+                parkour.start(context.player.uniqueId)
+            }
+        }
         render.layoutSlot('A', activeItem)
         render.layoutSlot('M', ownItem(render))
     }
