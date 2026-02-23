f3045e8a1a0b15bf3b5d55c4f663b2ea25fb546b
TheBjoRedCraft <thebjoredcraft@gmail.com>
2026-02-23 19:13:15 +0100

    feat: enhance SuccessListener to check surrounding blocks for success trigger

--- surf-parkour-paper/src/main/kotlin/dev/slne/surf/parkour/paper/listener/SuccessListener.kt
+++ surf-parkour-paper/src/main/kotlin/dev/slne/surf/parkour/paper/listener/SuccessListener.kt
@@ -4,11 +4,11 @@ import com.github.shynixn.mccoroutine.folia.entityDispatcher
 import com.github.shynixn.mccoroutine.folia.launch
 import dev.slne.surf.parkour.paper.plugin
 import dev.slne.surf.parkour.paper.service.parkourService
-import dev.slne.surf.parkour.paper.util.getBlocksBelowLocation
-import dev.slne.surf.parkour.paper.util.isSameBlock
+import org.bukkit.block.Block
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerMoveEvent
+import org.bukkit.util.Vector
 
 object SuccessListener : Listener {
     @EventHandler
@@ -28,12 +28,27 @@ object SuccessListener : Listener {
                 return@launch
             }
 
-            val blocksBelow = getBlocksBelowLocation(event.to)
+            val possibleBlocks = getBlocksAround(event.to.block)
             val targetBlock = generator.blockLocations.second
 
-            if (blocksBelow.any { it.isSameBlock(targetBlock) }) {
+            if (possibleBlocks.any { it.isSameBlock(targetBlock) }) {
                 parkourService.triggerSuccess(player)
             }
         }
     }
+
+    private fun Block.isSameBlock(other: Vector): Boolean {
+        return this.x == other.blockX && this.y == other.blockY && this.z == other.blockZ
+    }
+
+    private fun getBlocksAround(block: Block) = listOf(
+        block.getRelative(1, 0, 0),
+        block.getRelative(-1, 0, 0),
+        block.getRelative(0, 0, 1),
+        block.getRelative(0, 0, -1),
+        block.getRelative(1, 0, 1),
+        block.getRelative(1, 0, -1),
+        block.getRelative(-1, 0, 1),
+        block.getRelative(-1, 0, -1)
+    )
 }
