5327873ec90409b5bfe9520feac1a43008fb048d
TheBjoRedCraft <thebjoredcraft@gmail.com>
2026-04-01 18:09:50 +0200

    feat: mark surfParkourApi as deprecated and recommend using companion object

--- surf-parkour-api/src/main/kotlin/dev/slne/surf/parkour/api/SurfParkourApi.kt
+++ surf-parkour-api/src/main/kotlin/dev/slne/surf/parkour/api/SurfParkourApi.kt
@@ -3,7 +3,8 @@ package dev.slne.surf.parkour.api
 import dev.slne.surf.surfapi.core.api.util.requiredService
 import java.util.*
 
-private val surfParkourApi = requiredService<SurfParkourApi>()
+@Deprecated("This API is deprecated and will be removed in future versions. Please use companion object instead.")
+val surfParkourApi = requiredService<SurfParkourApi>()
 
 interface SurfParkourApi {
     suspend fun showGui(playerUuid: UUID)
